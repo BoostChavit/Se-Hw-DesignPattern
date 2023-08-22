380949c90363a1346cf0aa5e6b66d071f8aed0e7
Siwakorn Pasawang <siwakorn.pas@ku.th>
2023-08-22 15:22:46 +0700

    Add feature QuackEcho and edit DuckSimulator file

--- src/main/java/ku/cs/duck/DuckSimulator.java
+++ src/main/java/ku/cs/duck/DuckSimulator.java
@@ -11,15 +11,24 @@ public class DuckSimulator {
     }
 
     void simulate(){
-        Quackable mallardDuck = new QuackCounter(new MallardDuck());
-        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-        Quackable duckCall = new QuackCounter(new DuckCall());
-        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+        // QuackCounter
+//        Quackable mallardDuck = new QuackCounter(new MallardDuck());
+//        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+//        Quackable duckCall = new QuackCounter(new DuckCall());
+//        Quackable rubberDuck = new QuackCounter(new RubberDuck());
 //        Goose goose = new Goose();
 //        Pigeon pigeon = new Pigeon();
 
+        // QuackEcho
+        Quackable mallardDuck = new QuackEcho(new MallardDuck());
+        Quackable redheadDuck = new QuackEcho(new RedheadDuck());
+        Quackable duckCall = new QuackEcho(new DuckCall());
+        Quackable rubberDuck = new QuackEcho(new RubberDuck());
+
+
         // System.out.println("Duck Simulator");
-        System.out.println("Duck Simulator: with Decorator");
+        // System.out.println("Duck Simulator: with Decorator");
+        System.out.println("Duck Simulator: with Decorator ( QuackEcho )");
 
         simulate(mallardDuck);
         simulate(redheadDuck);
@@ -28,6 +37,6 @@ public class DuckSimulator {
 //        simulate(new GooseAdapter(goose));
 //        simulate(new PigeonAdapter(pigeon));
 
-        System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times.");
+        // System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times.");
     }
 }
